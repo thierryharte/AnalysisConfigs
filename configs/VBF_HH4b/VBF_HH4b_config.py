e9e65552df69ec009de7125b52772266ae5f0b16
matteomalucchi <matteomalucchi13@gmail.com>
2025-04-10 16:15:40 +0200

    Update onnx_executor.py and add new files for VBF

--- configs/VBF_HH4b/VBF_HH4b_config.py
+++ configs/VBF_HH4b/VBF_HH4b_config.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from collections import defaultdict
 
 from pocket_coffea.utils.configurator import Configurator
@@ -44,6 +43,7 @@ from configs.HH4b_common.dnn_input_variables import (
     bkg_morphing_dnn_input_variables_altOrder,
     sig_bkg_dnn_input_variables,
 )
+from configs.VBF_HH4b.onnx_models import onnx_model_dict
 
 
 localdir = os.path.dirname(os.path.abspath(__file__))
@@ -63,37 +63,19 @@ parameters = defaults.merge_parameters_from_files(
     update=True,
 )
 
-common_params = f"{localdir}/../HH4b_common/params_common/"
-
-onnx_model_dict = {
-    "SPANET": f"{common_params}/hh4b_5jets_e300_s100_ptvary_wide_loose_btag.onnx", # pt vary 0.3, 1.7
-    # "SPANET": "params/out_hh4b_5jets_ATLAS_ptreg_c0_lr1e4_wp0_noklininp_oc_300e_kl3p5.onnx",
-    "VBF_GGF_DNN": "",
-    # "VBF_GGF_DNN":"/t3home/rcereghetti/ML_pytorch/out/20241212_223142_SemitTightPtLearningRateConstant/models/model_28.onnx",
-    "BKG_MORPHING_DNN": f"{common_params}/DNN_AN_1e-3_e20drop75_minDelta1em5_SPANet_noEarlyStopping_20folds_average_model_from_onnx.onnx",  # 20 k-folds, early stopping, 1e-5 minDelta, spanet
-    # "BKG_MORPHING_DNN": "/t3home/mmalucch/ML_pytorch/out/DNN_AN_VR1_1e-3_e20drop75_minDelta1em5_SPANet_oversample_split/state_dict/average_model_from_onnx.onnx",  # VR1 train, early stopping, 1e-5 minDelta, spanet
-    # "BKG_MORPHING_DNN": "/work/mmalucch/out_ML_pytorch/DNN_AN_1e-3_e20drop75_minDelta1em5_run2/state_dict/average_model_from_onnx.onnx",  # Run2 CR train, early stopping, 1e-5 minDelta
-    # "BKG_MORPHING_DNN": "/work/mmalucch/out_ML_pytorch/DNN_AN_minDelta1em5/batch06/best_models/average_model_from_onnx.onnx",  # 20 k-folds, early stopping, 1e-5 minDelta, spanet
-    # "BKG_MORPHING_DNN": "/work/tharte/datasets/ML_pytorch/out/AN_1e-2_noDropout_e20lrdrop95/state_dict/ratio/average_model_from_onnx.onnx",
-    # "BKG_MORPHING_DNN": "/work/tharte/datasets/ML_pytorch/out/test_multiple_coffea/state_dict/model_40_state_dict.onnx", # thierry's model trained on 22C-22D-22E
-    # "BKG_MORPHING_DNN": "/work/tharte/datasets/ML_pytorch/out/batch01/best_models/output/average_model_from_onnx.onnx", # thierry's model trained on 22E
-    # "BKG_MORPHING_DNN": "/pnfs/psi.ch/cms/trivcat/store/user/mmalucch/keras_models_morphing/average_model_from_keras.onnx", # soumya's model
-    "SIG_BKG_DNN": "",
-    # "SIG_BKG_DNN": "/pnfs/psi.ch/cms/trivcat/store/user/mmalucch/keras_models_SvsB/model_fold0.onnx",
-}
 
 print("onnx_model_dict", onnx_model_dict)
 
 
-HIGGS_PARTON_MATCHING = False
 VBF_PARTON_MATCHING = False
 TIGHT_CUTS = False
 CLASSIFICATION = False
 SAVE_CHUNK = False
 VBF_PRESEL = False
 SEMI_TIGHT_VBF = True
-DNN_VARIABLES = True
-RUN2 = True
+
+DNN_VARIABLES = False
+RUN2 = False
 VR1 = False
 
 
@@ -118,7 +100,6 @@ if SAVE_CHUNK:
     # workflow_options["dump_columns_as_arrays_per_chunk"] = "root://t3dcachedb03.psi.ch:1094//pnfs/psi.ch/cms/trivcat/store/user/tharte/HH4b/training_samples/GluGlutoHHto4B_spanet_loose_03_17"
     pass
 
-jet_info = ["index", "pt", "btagPNetQvG", "eta", "btagPNetB", "phi", "mass"]
 
 # variables_dict = get_variables_dict(
 #     CLASSIFICATION=CLASSIFICATION,
@@ -127,16 +108,23 @@ jet_info = ["index", "pt", "btagPNetQvG", "eta", "btagPNetB", "phi", "mass"]
 # )
 variables_dict = {}
 
+if DNN_VARIABLES:
+    total_input_variables = (
+        sig_bkg_dnn_input_variables | bkg_morphing_dnn_input_variables
+    )
+    print(total_input_variables)
 
-total_input_variables = sig_bkg_dnn_input_variables | bkg_morphing_dnn_input_variables
-print(total_input_variables)
+    column_list = create_DNN_columns_list(
+        False, not SAVE_CHUNK, total_input_variables, btag=False
+    )
+    column_listRun2 = create_DNN_columns_list(
+        True, not SAVE_CHUNK, total_input_variables, btag=False
+    )
 
-column_list = create_DNN_columns_list(
-    False, not SAVE_CHUNK, total_input_variables, btag=False
-)
-column_listRun2 = create_DNN_columns_list(
-    True, not SAVE_CHUNK, total_input_variables, btag=False
-)
+else:
+    column_list = get_columns_list()
+    column_listRun2 = get_columns_list()
+
 
 preselection = (
     [vbf_hh4b_presel if TIGHT_CUTS is False else vbf_hh4b_presel_tight]
@@ -145,29 +133,23 @@ preselection = (
 )
 
 sample_list = [
-    "DATA_JetMET_JMENano_C_skimmed",
-    "DATA_JetMET_JMENano_D_skimmed",
-    "DATA_JetMET_JMENano_E_skimmed",
-    "DATA_JetMET_JMENano_F_skimmed",
-    "DATA_JetMET_JMENano_G_skimmed",
-    # "VBF_HHto4B",
+    # "DATA_JetMET_JMENano_C_skimmed",
+    # "DATA_JetMET_JMENano_D_skimmed",
+    # "DATA_JetMET_JMENano_E_skimmed",
+    # "DATA_JetMET_JMENano_F_skimmed",
+    # "DATA_JetMET_JMENano_G_skimmed",
     "GluGlutoHHto4B",
+    # "VBF_HHto4B",
 ]
 if not VR1:
     categories_dict = {
         "4b_control_region": [hh4b_4b_region, hh4b_control_region],
-        "2b_control_region_preW": [hh4b_2b_region, hh4b_control_region],
-        "2b_control_region_postW": [hh4b_2b_region, hh4b_control_region],
-        "4b_control_regionRun2": [hh4b_4b_region, hh4b_control_region_run2],
-        "2b_control_region_preWRun2": [hh4b_2b_region, hh4b_control_region_run2],
-        "2b_control_region_postWRun2": [hh4b_2b_region, hh4b_control_region_run2],
-        #
-        "4b_signal_region": [hh4b_4b_region, hh4b_signal_region],
-        "2b_signal_region_preW": [hh4b_2b_region, hh4b_signal_region],
-        "2b_signal_region_postW": [hh4b_2b_region, hh4b_signal_region],
-        "4b_signal_regionRun2": [hh4b_4b_region, hh4b_signal_region_run2],
-        "2b_signal_region_preWRun2": [hh4b_2b_region, hh4b_signal_region_run2],
-        "2b_signal_region_postWRun2": [hh4b_2b_region, hh4b_signal_region_run2],
+        # "2b_control_region_preW": [hh4b_2b_region, hh4b_control_region],
+        # "2b_control_region_postW": [hh4b_2b_region, hh4b_control_region],
+        # #
+        # "4b_signal_region": [hh4b_4b_region, hh4b_signal_region],
+        # "2b_signal_region_preW": [hh4b_2b_region, hh4b_signal_region],
+        # "2b_signal_region_postW": [hh4b_2b_region, hh4b_signal_region],
         #
         # "4b_region": [hh4b_4b_region],
         # "2b_region": [hh4b_2b_region],
@@ -191,28 +173,48 @@ if not VR1:
         # **{f"4b_VBF_0{i}qvg_region": [hh4b_4b_region, VBF_region, qvg_regions[f"qvg_0{i}_region"]] for i in range(5, 10)},
         # **{f"4b_VBF_0{i}qvg_generalSelection_region": [hh4b_4b_region, VBF_generalSelection_region, qvg_regions[f"qvg_0{i}_region"]] for i in range(5, 10)},
     }
+    if RUN2:
+        categories_dictRun2 = {
+            "4b_control_regionRun2": [hh4b_4b_region, hh4b_control_region_run2],
+            "2b_control_region_preWRun2": [hh4b_2b_region, hh4b_control_region_run2],
+            "2b_control_region_postWRun2": [hh4b_2b_region, hh4b_control_region_run2],
+            "4b_signal_regionRun2": [hh4b_4b_region, hh4b_signal_region_run2],
+            "2b_signal_region_preWRun2": [hh4b_2b_region, hh4b_signal_region_run2],
+            "2b_signal_region_postWRun2": [hh4b_2b_region, hh4b_signal_region_run2],
+        }
+        categories_dict = categories_dict | categories_dictRun2
 else:
     categories_dict = {
         "4b_VR1_control_region": [hh4b_4b_region, hh4b_VR1_control_region],
         "2b_VR1_control_region_preW": [hh4b_2b_region, hh4b_VR1_control_region],
         "2b_VR1_control_region_postW": [hh4b_2b_region, hh4b_VR1_control_region],
-        "4b_VR1_control_regionRun2": [hh4b_4b_region, hh4b_VR1_control_region_run2],
-        "2b_VR1_control_region_preWRun2": [
-            hh4b_2b_region,
-            hh4b_VR1_control_region_run2,
-        ],
-        "2b_VR1_control_region_postWRun2": [
-            hh4b_2b_region,
-            hh4b_VR1_control_region_run2,
-        ],
         #
         "4b_VR1_signal_region": [hh4b_4b_region, hh4b_VR1_signal_region],
         "2b_VR1_signal_region_preW": [hh4b_2b_region, hh4b_VR1_signal_region],
         "2b_VR1_signal_region_postW": [hh4b_2b_region, hh4b_VR1_signal_region],
-        "4b_VR1_signal_regionRun2": [hh4b_4b_region, hh4b_VR1_signal_region_run2],
-        "2b_VR1_signal_region_preWRun2": [hh4b_2b_region, hh4b_VR1_signal_region_run2],
-        "2b_VR1_signal_region_postWRun2": [hh4b_2b_region, hh4b_VR1_signal_region_run2],
     }
+    if RUN2:
+        categories_dictRun2 = {
+            "4b_VR1_control_regionRun2": [hh4b_4b_region, hh4b_VR1_control_region_run2],
+            "2b_VR1_control_region_preWRun2": [
+                hh4b_2b_region,
+                hh4b_VR1_control_region_run2,
+            ],
+            "2b_VR1_control_region_postWRun2": [
+                hh4b_2b_region,
+                hh4b_VR1_control_region_run2,
+            ],
+            "4b_VR1_signal_regionRun2": [hh4b_4b_region, hh4b_VR1_signal_region_run2],
+            "2b_VR1_signal_region_preWRun2": [
+                hh4b_2b_region,
+                hh4b_VR1_signal_region_run2,
+            ],
+            "2b_VR1_signal_region_postWRun2": [
+                hh4b_2b_region,
+                hh4b_VR1_signal_region_run2,
+            ],
+        }
+        categories_dict = categories_dict | categories_dictRun2
 
 
 bycategory_column_dict = {}
@@ -243,11 +245,12 @@ cfg = Configurator(
     parameters=parameters,
     datasets={
         "jsons": [
-            f"{localdir}/datasets/QCD.json",
+            # f"{localdir}/datasets/QCD.json",
             f"{localdir}/datasets/signal_VBF_HH4b.json",
             # f"{localdir}/datasets/signal_ggF_HH4b_local.json",
-            # f"{localdir}/datasets/signal_ggF_HH4b_local_rucio_redirector.json",
+            # f"{localdir}/datasets/signal_ggF_HH4b_local_rucio.json",
             f"{localdir}/datasets/signal_ggF_HH4b_altSite.json",
+            # f"{localdir}/datasets/signal_ggF_HH4b_test.json",
             f"{localdir}/datasets/DATA_JetMET_skimmed.json",
         ],
         "filter": {
@@ -264,8 +267,8 @@ cfg = Configurator(
     ],
     preselections=preselection,
     categories=categories_dict,
-    weights_classes=common_weights
-    + [bkg_morphing_dnn_weight, bkg_morphing_dnn_weightRun2],
+    # weights_classes=common_weights
+    # + [bkg_morphing_dnn_weight, bkg_morphing_dnn_weightRun2],
     weights={
         "common": {
             "inclusive": [
@@ -275,7 +278,7 @@ cfg = Configurator(
             ],
             "bycategory": {},
         },
-        "bysample": bysample_bycategory_weight_dict,
+        "bysample": {}, #bysample_bycategory_weight_dict,
     },
     variations={
         "weights": {
