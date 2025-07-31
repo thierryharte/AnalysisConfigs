678f9028241ca13e743b0d729466d7458357c3be
matteomalucchi <matteomalucchi13@gmail.com>
2025-07-31 15:49:08 +0200

    update configs

---
+++ configs/HH4b_common/config_files/spanet_ptflat_inclusive_b_trained_bratio_ArctanhDeltaProb_MorphingSvBvariables.py
@@ -0,0 +1,47 @@
+from configs.HH4b_common.dnn_input_variables import (
+    sig_bkg_dnn_ArctanhDeltaProb_input_variables,
+)
+
+onnx_model_dict = {
+    "spanet": "",
+    "vbf_ggf_dnn": "",
+    "bkg_morphing_dnn": "",
+    "sig_bkg_dnn": "",
+    "bkg_morphing_spread_dnn": "",
+}
+
+
+onnx_model_dict |= {
+    "spanet": "/work/tharte/datasets/onnx_spanet_models_for_pairing_and_mass_sculpting_studies/spanet_hh4b_inclusive_b_region_5jets_loose_ptvary_wide_bratio_300_inclusive_years_s100.onnx",  # spanet pt vary 0.3, 1.7
+    "bkg_morphing_dnn": "/work/mmalucch/out_ML_pytorch/DNN_AN_1e-3_e20drop75_minDelta1em5_SPANet_inclusive_b_region_bratioAll_postEE_ArctanhDeltaProb_SvBvariables/best_models/average_model_from_onnx.onnx",  # only 2022_postEE, 20 k-folds, early stopping, 1e-5 minDelta, spanet pt vary
+    "sig_bkg_dnn": "/work/mmalucch/out_ML_pytorch/DNN_SvB_1e-3_e20drop75_minDelta1em5_SPANet_inclusive_b_region_bratioAll_postEE_ArctanhDeltaProb_MorphingSvBvariables/state_dict/model_best_epoch_11.onnx",
+}
+
+
+config_options_dict = {
+    "higgs_parton_matching": False,
+    "vbf_parton_matching": False,
+    "tight_cuts": False,
+    "classification": False,
+    "save_chunk": False,
+    "vbf_presel": False,
+    "semi_tight_vbf": True,
+    "dnn_variables": True,
+    "run2": False,
+    "vr1": False,
+    "random_pt": False,
+    "rand_type": 0.3,
+    "blind": True if onnx_model_dict["sig_bkg_dnn"] else False,
+    "bkg_morphing_dnn_input_variables": sig_bkg_dnn_ArctanhDeltaProb_input_variables,
+    "sig_bkg_dnn_input_variables": sig_bkg_dnn_ArctanhDeltaProb_input_variables,
+    "parton_jet_min_dR": 0.4,
+    "max_num_jets": 5,
+    "which_bquark": "last",
+    "fifth_jet": "pt",
+    "donotscale_sumgenweights": True,
+    "pad_value": -999.0,
+    "arctanh_delta_prob_bin_edge": 2.44,
+    "arctanh_delta_prob_pad_limit": 2.,
+    "add_jet_spanet": True,
+    "spanet_input_name_list": ["log_pt", "eta", "phi", "btag_ratioAll"],
+} | onnx_model_dict
